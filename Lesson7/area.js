c9d22198939bdbf6188092bf0b876df225c0efae
tuandung1111 <tuandung.ait.it@gmail.com>
2022-04-12 12:32:14 +0700

    add

---
+++ Lesson7/area.js
@@ -0,0 +1,9 @@
+let thewidth;
+let theheight;
+thewidth = prompt("Width is: ");
+theheight = prompt("Height is: ");
+let width = parseInt(thewidth);
+let height = parseInt(theheight);
+let area = width * height;
+
+document.write("Area = " + area);
